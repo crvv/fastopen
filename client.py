a08a3809e70e91765ab70575f4d0e608849a6917
crvv <crvv.pku@gmail.com>
2015-11-28 19:10:32 +0800

    tcp fastopen in client

--- client.py
+++ client.py
@@ -3,11 +3,13 @@
 # Echo client program
 import socket
 
+MSG_FASTOPEN = 0x20000000
+
 HOST = 'localhost'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.sendall(b'Hello, world')
+s.sendto(b'Hello, world', MSG_FASTOPEN, (HOST, PORT))
+s.sendall(b'  world')
 data = s.recv(1024)
 s.close()
 print('Received', repr(data))
