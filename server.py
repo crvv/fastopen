d668e54ff945ef4f0fc8e14f0eb5706d56224e62
crvv <crvv.pku@gmail.com>
2015-11-29 00:08:48 +0800

    fastopen tcp server on Darwin

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Echo server program
 import socket
@@ -7,16 +7,19 @@ HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
-s.listen(1)
+s.listen()
+s.setsockopt(socket.SOL_TCP, 0x105, 1)
+
 while True:
     conn, addr = s.accept()
+    conn.sendall(b'tcp fastopen server')
 
     print('Connected by', addr)
     while True:
         data = conn.recv(1024)
         if not data:
             break
-        conn.sendall(data)
+        print(data)
     conn.close()
 
 
