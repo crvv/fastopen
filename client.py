d611bf274a132f8947146a10673691bdb60b756e
crvv <crvv.pku@gmail.com>
2015-11-29 01:02:56 +0800

    fastopen tcp client on Darwin

--- client.py
+++ client.py
@@ -1,15 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Echo client program
 import socket
+import fastopen
 
 MSG_FASTOPEN = 0x20000000
 
 HOST = 'localhost'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.sendto(b'Hello, world', MSG_FASTOPEN, (HOST, PORT))
-s.sendall(b'  world')
+
+fastopen.connect(s.fileno(), HOST, PORT)
+
+s.send(b'Hello, world')
+
 data = s.recv(1024)
 s.close()
 print('Received', repr(data))
