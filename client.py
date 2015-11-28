eede3075ba72edd39ae94c768f66bf911f336aab
crvv <crvv.pku@gmail.com>
2015-11-28 11:04:12 +0000

    fix wrong hostname

--- client.py
+++ client.py
@@ -3,7 +3,7 @@
 # Echo client program
 import socket
 
-HOST = '192.168.1.138'    # The remote host
+HOST = 'localhost'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
